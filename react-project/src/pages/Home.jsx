6f1ec77208eecac6c7e01879e85fef3de60815e1
mahanandhan <mahanandhanmanish@gmail.com>
2025-02-08 12:05:20 +0530

    added

---
+++ react-project/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Name: Mahanandhan</h1>
+      <h1>contact no : 999999999</h1>
+    </div>
+  )
+}
+
+export default Home
